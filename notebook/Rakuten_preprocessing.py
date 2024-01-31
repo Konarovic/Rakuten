0f819a429d2796829d78c1657e464923eabd6dfc
Thibaud BENOIST <thibaud.benoist@ageval.fr>
2024-01-31 21:55:18 +0100

    wordcloud masks 1/2

--- notebook/Rakuten_preprocessing.py
+++ notebook/Rakuten_preprocessing.py
@@ -17,7 +17,7 @@ Usage:
     - data['description'] = rkt.Rakuten_txt_cleanup(data['description'])
 
     - data['language'] = rkt.Rakuten_txt_language(
-        data.loc[:, ['designation', 'description']])
+        data[['designation', 'description']])
 
     - data['description_tokens'] = rkt.Rakuten_txt_tokenize(
         data['description'], lang=data['language'])
@@ -34,15 +34,25 @@ Usage:
     - data['image_path'] = rkt.Rakuten_img_path('../Data/images/image_train/',
                                             data['imageid'], data['productid'])
     
-    - data = data.join(rkt.Rakuten_img_size(data['image_path']), axis=1)
+    - data = data.join(rkt.Rakuten_img_size(data['image_path']))
 
-    - df_words = pd.DataFrame()
-      for c in target['prdtypecode'].unique():
-        cnt, wrd = rkt.Rakuten_txt_wordcount(
-            data.[data['prdtypecode'] == c, 'designation_tokens'])
-        df_words = df_words.join(pd.DataFrame(
-            cnt, index=wrd, columns=['code_' + str(c)]))
-
+    - #Proportion of each tokens across all product of each category
+    df_words = pd.DataFrame()
+    for code in data['prdtypecode'].unique():
+        cnt, wrd = rkt.Rakuten_txt_wordcount(data.loc[data['prdtypecode'] == code, 'designation_tokens'])
+        cnt = cnt / (data['prdtypecode']==code).sum()
+        df_words = df_words.join(pd.DataFrame(cnt, index=wrd, columns=['code_' + str(code)]), how='outer')
+        df_words = df_words.fillna(0)
+        
+    from scipy.cluster.hierarchy import linkage, leaves_list
+    Z = linkage(df_words.corr(), 'ward')
+    order = leaves_list(Z)
+    px.imshow(df_words.corr().iloc[order, order])
+    
+    #multiplying the word frequency by the relative proportion of each token
+    across categories
+    df_words_rel = df_words * df_words.apply(lambda row: row / row.sum(), axis=1)
+    
 
 Dependencies:
     - pandas: Used for data manipulation and analysis.
@@ -56,17 +66,22 @@ Dependencies:
 """
 
 import cv2
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
 import re
 from collections import Counter
 from bs4 import BeautifulSoup
-import langid
+#import langid
+import py3langid as langid
+from spellchecker import SpellChecker
 import spacy
 import nltk
 import os
 import pandas as pd
-import swifter
 import numpy as np
+
+import unidecode
+from transformers import BertTokenizer
+import torch
 
 from plotly.subplots import make_subplots
 from plotly import graph_objects as go
@@ -74,6 +89,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.io as pio
+from PIL import Image
 pio.renderers.default = 'browser'
 
 
@@ -144,51 +160,68 @@ def Rakuten_txt_cleanup(data):
     # For a Series
     cleaned_series = Rakuten_txt_cleanup(series_with_text)
     """
+    # All regex to remove
+    subregex = []
+    # url patterns
+    subregex.append(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|\
+                   [!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    # filename patterns
+    subregex.append(r'\b(?<!\d\.)\w+\.(txt|jpg|png|docx|pdf)\b')
+    # badly formatted html markups
+    subregex.append(r'nbsp|&amp|& nbsp|')
+    # Converting subregex to regex pattern object
+    subregex = re.compile('|'.join(subregex), re.IGNORECASE)
 
-    url_regex = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|\
-                           [!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    filename_regex = re.compile(r'\b(?<!\d\.)\w+\.(txt|jpg|png|docx|pdf)\b')
+    # All regex to add space around
+    spacearound = []
+    # Add spaces around numbers and punctuations except -, ', / and ¿
+    spacearound.append(r'(\d+|[.,!;:(){}\[\]"@#$%^&*+=|<>~`¬])')
+    # Converting spacearound to regex pattern object
+    spacearound = re.compile('|'.join(spacearound))
+
+    # All regex to add space before
+    spacebefore = []
+    # Add spaces before uppercase letters if they're both preceded and followed
+    # by lowercase letters
+    spacebefore.append(r'(?<=[a-z])([A-Z])(?=[a-z])')
+    # Converting spacebefore to regex pattern object
+    spacebefore = re.compile('|'.join(spacebefore))
 
     if data.ndim == 2:
         # if data if a dataframe
         for col in data.columns:
             data.loc[:, col] = data[col].apply(
-                lambda row: txt_cleanup(row, url_regex, filename_regex))
-
+                lambda row: txt_cleanup(row, subregex, spacearound, spacebefore))
+            # Replacing empty strings with NaNs
             data.loc[data[col].astype(str).str.len() == 0, col] = np.nan
 
     else:
         # if data is a series
         data = data.apply(
-            lambda row: txt_cleanup(row, url_regex, filename_regex))
-
+            lambda row: txt_cleanup(row, subregex, spacearound, spacebefore))
+        # Replacing empty strings with NaNs
         data.loc[data.astype(str).str.len() == 0] = np.nan
 
     return data
 
 
-def txt_cleanup(txt, url_regex, filename_regex):
+def txt_cleanup(txt, subregex, spacearound, spacebefore):
     """
     Remove HTML tags, URLs, and filenames from a given text string.
 
     Parameters:
     txt (str): Text to be cleaned.
-    url_regex (compiled regex): Regex pattern to identify URLs.
-    filename_regex (compiled regex): Regex pattern to identify filenames.
+    subregex (compiled regex): Regex patterns to remove.
+    spacearound (compiled regex): Regex patterns to split.
+    spacebefore (compiled regex): Regex patterns where space should be added before.
 
     Returns:
-    str: Cleaned text with HTML tags, URLs, and filenames removed.
+    str: Cleaned text with HTML tags, URLs, filenames, etc removed.
 
     Usage:
-    cleaned_text = txt_cleanup(some_text, url_regex, filename_regex)
+    cleaned_text = txt_cleanup(some_text, subregex, splitregex)
     """
     if isinstance(txt, str):
-        # Remove URLs
-        txt = url_regex.sub(' ', txt)
-
-        # # remove filenames
-        txt = filename_regex.sub(' ', txt)
-
         # Remove HTML tags
         soup = BeautifulSoup(txt, 'html.parser')
         txt = soup.get_text(separator=' ')
@@ -197,6 +230,18 @@ def txt_cleanup(txt, url_regex, filename_regex):
         soup = BeautifulSoup(txt, 'lxml')
         txt = soup.get_text(separator=' ')
 
+        # Remove according to subregex
+        txt = subregex.sub(' ', txt)
+
+        # Split according to spacearound
+        txt = spacearound.sub(r' \1 ', txt)
+
+        # Add space before according to spacebefore
+        txt = spacebefore.sub(r' \1', txt)
+
+        # cleaning up extra spaces
+        txt = re.sub(r'\s+', ' ', txt).strip()
+
         # removing all text shorter than 4 characters (eg ..., 1), -, etc)
         if len(txt.strip()) < 4:
             txt = ''
@@ -204,7 +249,8 @@ def txt_cleanup(txt, url_regex, filename_regex):
     return txt
 
 
-def Rakuten_txt_language(data):  # ajouter une condition sur le score de fin
+# ajouter une condition sur le score de fin
+def Rakuten_txt_language(data, method='langid'):
     """
     Detect the most likely language of text data in each row of a DataFrame or
     Series.
@@ -226,11 +272,42 @@ def Rakuten_txt_language(data):  # ajouter une condition sur le score de fin
         data = data.apply(
             lambda row: ' '.join([s for s in row.loc[:]
                                   if isinstance(s, str)]), axis=1)
+    # Replacing NaNs with empty string
+    data = data.fillna(' ')
 
     # getting the most likely language for each row of the data series
     # langid.classify(row) returns ('language', score). We only keep the
     # language here
-    lang = data.apply(lambda row: langid.classify(row)[0])
+    if method == 'langid':
+        lang = data.apply(lambda row: langid.classify(row)[0])
+
+    elif method == 'pyspell':
+        spell_fr = SpellChecker(language='fr', distance=1)
+        spell_en = SpellChecker(language='en', distance=1)
+        spell_de = SpellChecker(language='de', distance=1)
+
+        err_fr = data.apply(lambda row: len(spell_fr.known(row.split())))
+        err_en = data.apply(lambda row: len(spell_en.known(row.split())))
+        err_de = data.apply(lambda row: len(spell_de.known(row.split())))
+        lang = pd.concat([err_fr.rename('fr'), err_en.rename(
+            'en'), err_de.rename('de')], axis=1)
+        lang = lang.idxmax(axis=1)
+
+    elif method == 'bert':
+        tokenizer_en = BertTokenizer.from_pretrained('bert-base-uncased')
+        tokenizer_fr = BertTokenizer.from_pretrained(
+            'dbmdz/bert-base-french-europeana-cased')
+        tokenizer_de = BertTokenizer.from_pretrained('bert-base-german-cased')
+
+        err_fr = data.apply(lambda row: ' '.join(
+            tokenizer_fr.convert_ids_to_tokens(tokenizer_fr(row)['input_ids'])))
+        err_en = data.apply(lambda row: ' '.join(
+            tokenizer_en.convert_ids_to_tokens(tokenizer_en(row)['input_ids'])))
+        err_de = data.apply(lambda row: ' '.join(
+            tokenizer_de.convert_ids_to_tokens(tokenizer_de(row)['input_ids'])))
+        lang = pd.concat([err_fr.rename('fr'), err_en.rename(
+            'en'), err_de.rename('de')], axis=1)
+        lang = lang.idxmin(axis=1)
 
     return lang
 
@@ -244,7 +321,6 @@ def Rakuten_txt_tokenize(data, lang=None, method='spacy'):
     data (DataFrame or Series): Text data to tokenize and lemmatize.
     lang (Series, optional): Series containing language information for each
     entry.
-    method: 'spacy', 'spacy_accuracy' or 'nltk'. Method to use for tokenization and lemmatization.
 
     Returns:
     DataFrame or Series: Data with tokenized and lemmatized text.
@@ -264,24 +340,21 @@ def Rakuten_txt_tokenize(data, lang=None, method='spacy'):
     # joining text and language data
     data = pd.concat([data, lang], axis=1)
 
-    if (method == 'spacy') or (method == 'spacy_accuracy'):
+    if method == 'spacy':
         # Checking if spacy language data have been  downloaded
-        suffix = 'trf' if method == 'spacy_accuracy' else 'sm'
-        core = 'dep' if method == 'spacy_accuracy' else 'core'
+        if not spacy.util.is_package('en_core_web_sm'):
+            spacy.cli.download('en_core_web_sm')
 
-        if not spacy.util.is_package('en_core_web_'+suffix):
-            spacy.cli.download('en_core_web_'+suffix)
+        if not spacy.util.is_package('fr_core_news_sm'):
+            spacy.cli.download('fr_core_news_sm')
 
-        if not spacy.util.is_package('fr_'+core+'_news_'+suffix):
-            spacy.cli.download('fr_'+core+'_news_'+suffix)
-
-        if not spacy.util.is_package('de_'+core+'_news_'+suffix):
-            spacy.cli.download('de_'+core+'_news_'+suffix)
+        if not spacy.util.is_package('de_core_news_sm'):
+            spacy.cli.download('de_core_news_sm')
 
         # Loading spacy language models
-        nlpdict = {'en': spacy.load('en_core_web_'+suffix),
-                'fr': spacy.load('fr_'+core+'_news_'+suffix),
-                'de': spacy.load('de_'+core+'_news_'+suffix)}
+        nlpdict = {'en': spacy.load('en_core_web_sm'),
+                   'fr': spacy.load('fr_core_news_sm'),
+                   'de': spacy.load('de_core_news_sm')}
 
         # Applying tokenisation using spacy
         data = data.apply(lambda row: tokens_from_spacy(
@@ -306,6 +379,10 @@ def Rakuten_txt_tokenize(data, lang=None, method='spacy'):
     return data
 
 
+def Rakuten_txt_translate(txt):
+    """  """
+
+
 def tokens_from_spacy(txt, lang, nlpdict):
     """
     Generate a list of unique word tokens from a text string using spaCy.
@@ -328,8 +405,10 @@ def tokens_from_spacy(txt, lang, nlpdict):
         # Remove stopwords, punctuation, and perform lemmatization
         filtered_tokens = [token.lemma_.lower()
                            for token in tokens
-                           if token.is_alpha and not token.is_stop
-                           and len(token) > 3]
+                           if token.is_alpha
+                           and not token.is_stop
+                           and len(token) > 2
+                           and any(vowel in token.text.lower() for vowel in 'aeiouyáéíóúàèìòùâêîôûäëïöü')]
 
         # Keeping a unique list of tokens, in the same order they appeared in
         # the text
@@ -368,8 +447,10 @@ def tokens_from_nltk(txt, lang):
         filtered_tokens = [stemmer.stem(token.lower())
                            for token in tokens
                            if token.isalpha()
-                           and token.lower() not in stop_words
-                           and len(token) > 3]
+                           and token.lower()
+                           not in stop_words
+                           and len(token) > 2
+                           and any(vowel in token.text.lower() for vowel in 'aeiouyáéíóúàèìòùâêîôûäëïöü')]
 
         # Keeping a unique list of tokens, in the same order they appeared in
         # the text
@@ -456,6 +537,37 @@ def Rakuten_txt_wordcount(data, nmax_words=None, Normalize=False):
     return countlist, wordlist
 
 
+def Rakuten_txt_wordcloud(data, token_col_name, categories):
+    plt_rows = len(categories)
+    plt_idx = 0
+    fig, axs = plt.subplots(plt_rows, 1, figsize=(12, 6*plt_rows))
+    stopwords = set(STOPWORDS)
+    for code in categories.index:
+        img = Image.open('./wordcloud-masks/console.jpg')
+        mask_coloring = np.array(img)
+        wordcloud = WordCloud(
+                        background_color='white',
+                        mask=mask_coloring,
+                        min_font_size=5,
+                        max_font_size=30,
+                        contour_width=1,
+                        random_state=42,
+                        max_words=4000,
+                        stopwords=stopwords,
+                    ).generate(' '.join(data[data.prdtypecode == code][token_col_name]))
+        # img_colors=ImageColorGenerator(mask_coloring)
+        # axs[plt_idx].imshow(wordcloud.recolor(color_func=img_colors), interpolation="bilinear")
+        wc_img = Image.fromarray(wordcloud.to_array())
+        # back_img= img.resize(wc_img.size)
+        # img_new = Image.alpha_composite(back_img, wc_img)
+        axs[plt_idx].imshow(wc_img)
+        axs[plt_idx].set_title(str(code) + ' ' + categories.loc[code][0])
+        axs[plt_idx].axis("off")
+        plt_idx += 1
+
+    plt.tight_layout()
+    plt.show()
+
 def Rakuten_img_path(img_folder, imageid, productid):
     """ retrurns the path to the image of a given productid and imageid"""
 
@@ -527,3 +639,95 @@ def get_img_size(img_path):
         width_actual, height_actual = 0, 0
 
     return [width, height, width_actual, height_actual]
+
+
+def img_resize(folder_path, save_path='./resized/'):
+    """
+    Resize image files in a specified folder to remove extra padding areas and resize the image
+
+    Parameters:
+        folder_path (str): The path to the folder containing the image files to be resized.
+        save_path (str, optional): The path to the folder where the resized images will be saved.
+                                   Default is './resized/'.
+
+    Returns:
+        None
+
+    This function iterates over all image files in the specified folder, resizes each image
+    while removing extra padding areas, and saves the resized images in the 'save_path' folder.
+    The resized images are named with '_resized' suffix and have the same file format as the
+    original images.
+
+    Example:
+        img_resize('/path/to/images_folder', '/path/to/save_resized_images/')
+
+    Note:
+        - The function assumes that the input images are in JPEG format (.jpg).
+        - The 'save_path' folder will be created if it does not exist.
+    """
+
+    # list of all files
+    all_files = os.listdir(folder_path)
+
+    # Image files
+    image_files = [f for f in all_files if f.lower().endswith(('.jpg'))]
+
+    # Making sure save_path exists
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # Iterating over images
+    for img_name in image_files:
+        # Reading the image
+        img = cv2.imread(os.path.join(folder_path, img_name))
+
+        # full size of the image
+        width, height = img.shape[:2]
+
+        # converting to gray scale to threshold white padding
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Padding the gray image with a white rectangle around the full image to
+        # make sure there is at least this contour to find
+        border_size = 1
+        gray = cv2.copyMakeBorder(gray, border_size, border_size, border_size,
+                                  border_size, cv2.BORDER_CONSTANT,
+                                  value=[255, 255, 255])
+
+        # Threshold the image to get binary image (white pixels will be black)
+        _, thresh = cv2.threshold(gray, 254, 255, cv2.THRESH_BINARY_INV)
+
+        # Finding the contours of the non-white area
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            # Getting the bounding rectangle for the largest contour, if contours
+            # is not empty
+            x, y, width_actual, height_actual = cv2.boundingRect(
+                max(contours, key=cv2.contourArea))
+
+            # Compute scaling factors along x and y depending on the largest dim
+            if width_actual >= height_actual:
+                scale_x = width / width_actual
+                scale_y = scale_x
+            else:
+                scale_y = height / height_actual
+                scale_x = scale_y
+
+            # Cropping and resizing the image
+            img = img[y:y+height_actual, x:x+width_actual]
+            img = cv2.resize(img, (0, 0), fx=scale_x, fy=scale_y)
+
+            # Padding the image with white to reach original dimension
+            # (usually 500 x 500)
+            pad_top = (height - img.shape[0]) // 2
+            pad_bottom = height - img.shape[0] - pad_top
+            pad_left = (width - img.shape[1]) // 2
+            pad_right = width - img.shape[1] - pad_left
+            img = cv2.copyMakeBorder(img, pad_top, pad_bottom, pad_left,
+                                     pad_right, cv2.BORDER_CONSTANT,
+                                     value=[255, 255, 255])
+        # Saving the resized image to the same folder with the suffix "_resized"
+        output_path = os.path.join(
+            save_path, os.path.splitext(img_name)[0] + '_resized.jpg')
+        cv2.imwrite(output_path, img)
