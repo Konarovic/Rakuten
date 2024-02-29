5a2245c590b44d4c24d0bc47570de02ab517e154
Manggy94 <alex.mangwa@gmail.com>
2024-02-29 01:01:29 +0100

    :sparkles: Ajout des transformers de détection de langue

--- src/features/text/transformers/languages.py
+++ src/features/text/transformers/languages.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 import py3langid as langid
 from spellchecker import SpellChecker
+from transformers import BertTokenizer
 
 def Rakuten_txt_language(data, method='langid'):
     """
@@ -31,24 +32,9 @@ def Rakuten_txt_language(data, method='langid'):
     # getting the most likely language for each row of the data series
     # langid.classify(row) returns ('language', score). We only keep the
     # language here
-    if method == 'langid':
-        # Subsetting possible languages
-        langid.set_languages(['fr', 'en', 'de'])
-        lang = data.apply(lambda row: langid.classify(row)[0])
+   
 
-    elif method == 'pyspell':
-        spell_fr = SpellChecker(language='fr', distance=1)
-        spell_en = SpellChecker(language='en', distance=1)
-        spell_de = SpellChecker(language='de', distance=1)
-
-        err_fr = data.apply(lambda row: len(spell_fr.known(row.split())))
-        err_en = data.apply(lambda row: len(spell_en.known(row.split())))
-        err_de = data.apply(lambda row: len(spell_de.known(row.split())))
-        lang = pd.concat([err_fr.rename('fr'), err_en.rename(
-            'en'), err_de.rename('de')], axis=1)
-        lang = lang.idxmax(axis=1)
-
-    elif method == 'bert':
+    if method == 'bert':
         tokenizer_en = BertTokenizer.from_pretrained('bert-base-uncased')
         tokenizer_fr = BertTokenizer.from_pretrained(
             'dbmdz/bert-base-french-europeana-cased')
@@ -81,5 +67,48 @@ class LangIdDetector(BaseEstimator, TransformerMixin):
         lang = X.apply(lambda row: langid.classify(row)[0])
         return lang
 
+
 class PyspellDetector(BaseEstimator, TransformerMixin):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        
+        spell_fr = SpellChecker(language='fr', distance=1)
+        spell_en = SpellChecker(language='en', distance=1)
+        spell_de = SpellChecker(language='de', distance=1)
+
+        err_fr = X.apply(lambda row: len(spell_fr.known(row.split())))
+        err_en = X.apply(lambda row: len(spell_en.known(row.split())))
+        err_de = X.apply(lambda row: len(spell_de.known(row.split())))
+        lang = pd.concat([err_fr.rename('fr'), err_en.rename(
+            'en'), err_de.rename('de')], axis=1)
+        lang = lang.idxmax(axis=1)
+        return lang
+
+class BertDetector(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        tokenizer_en = BertTokenizer.from_pretrained('bert-base-uncased')
+        tokenizer_fr = BertTokenizer.from_pretrained(
+            'dbmdz/bert-base-french-europeana-cased')
+        tokenizer_de = BertTokenizer.from_pretrained('bert-base-german-cased')
+
+        err_fr = X.apply(lambda row: ' '.join(
+            tokenizer_fr.convert_ids_to_tokens(tokenizer_fr(row)['input_ids'])))
+        err_en = X.apply(lambda row: ' '.join(
+            tokenizer_en.convert_ids_to_tokens(tokenizer_en(row)['input_ids'])))
+        err_de = X.apply(lambda row: ' '.join(
+            tokenizer_de.convert_ids_to_tokens(tokenizer_de(row)['input_ids'])))
+        lang = pd.concat([err_fr.rename('fr'), err_en.rename(
+            'en'), err_de.rename('de')], axis=1)
+        lang = lang.idxmin(axis=1)
+        return lang
