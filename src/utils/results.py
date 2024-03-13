dbc9eac68637933a23c72ff08d8be80511df2ebf
Thibaud BENOIST <thibaud.benoist@ageval.fr>
2024-03-13 20:11:49 +0100

    Adds ResultsManager.plot_classification_report

--- src/utils/results.py
+++ src/utils/results.py
@@ -2,16 +2,19 @@ import pandas as pd
 import src.utils.plot as uplot
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-import config
 import ast
 import os
 import plotly.express as px
+import re
+from src.utils.load import load_classifier
 
 
 class ResultsManager():
-    def __init__(self) -> None:
+    def __init__(self, config) -> None:
+        self.config = config
         self.df_results = None
         self.le = None
+        self.X_test = None
         pass
 
     def add_result_file(self, file_path, package):
@@ -103,46 +106,55 @@ class ResultsManager():
         if self.le is None:
             self.le = LabelEncoder()
             self.le.classes_ = np.load(os.path.join(
-                config.path_to_data, 'le_classes.npy'), allow_pickle=True)
+                self.config.path_to_data, 'le_classes.npy'), allow_pickle=True)
 
         return self.le.classes_
 
+    def plot_classification_report(self, model_path):
+        y_pred = self.get_y_pred(model_path)
+        y_test = self.get_y_test(model_path)
 
-# def plot_bench_results_cat(self, data, x_column, y_column, x_label, y_label, color_column=None, title=None):
+        uplot.classification_results(
+            y_test, y_pred, index=self.get_cat_labels())
+        return self
 
-#     custom_categories_order = data[x_column].unique().tolist()
+    def get_y_pred(self, model_path):
+        if pd.isna(self.df_results[self.df_results.model_path == model_path].pred_test.values[0]):
+            clf = load_classifier(model_path)
+            y_pred = clf.predict(self.get_X_test())
+            return y_pred
+        pred = self.df_results[self.df_results.model_path ==
+                               model_path].pred_test.values[0]
 
-#     for i, cat in enumerate(custom_categories_order):
-#         custom_categories_order[i] = self.le.inverse_transform(
-#             ast.literal_eval(cat
+        return ast.literal_eval(pred)
 
-#     fig = px.bar(
-#         data,
-#         y=x_column,
-#         x=y_column,
-#         color=color_column,
-#         color_discrete_sequence=px.colors.qualitative.Plotly,
-#         category_orders={x_column: custom_categories_order},
-#     )
+    def get_y_test(self, model_path):
+        if pd.isna(self.df_results[self.df_results.model_path == model_path].y_test.values[0]):
+            df = pd.read_csv(os.path.join(
+                self.config.path_to_data, 'df_test_index.csv'))
+            return df.prdtypeindex.values
 
-#     fig.update_traces(
-#         width=0.8,
+        return ast.literal_eval(self.df_results[self.df_results.model_path == model_path].y_test.values[0])
 
-#     )
-#     # Update layout to remove legend and adjust xaxis title
-#     fig.update_layout(
-#         legend=None,
-#         xaxis_title=y_label,
-#         yaxis_title=x_label,
-#         bargap=0.3,
-#         bargroupgap=0.2,
-#         barmode='stack',
-#         width=1200,
-#         height=600,
-#         title=title
+    def get_X_test(self):
+        print(os.path.join(
+            self.config.path_to_data, 'df_test_index.csv'))
+        if self.X_test is None:
+            df = pd.read_csv(os.path.join(
+                self.config.path_to_data, 'df_test_index.csv'))
+            colnames = ['designation_translated', 'description_translated']
+            df['tokens'] = df[colnames].apply(lambda row: ' '.join(
+                s.lower() for s in row if isinstance(s, str)), axis=1)
+            df['img_path'] = df.apply(lambda row:
+                                      os.path.join(self.config.path_to_images, 'image_'
+                                                   + str(row['imageid'])
+                                                   + '_product_'
+                                                   + str(row['productid'])
+                                                   + '_resized'
+                                                   + '.jpg'),
+                                      axis=1)
+            self.X_test = df[['tokens', 'img_path']]
+        return self.X_test
 
-#     )
-
-#     # Show the plot
-#     fig.show()
-#     return fig
+    def get_model_paths(self):
+        return self.df_results.model_path.unique()
